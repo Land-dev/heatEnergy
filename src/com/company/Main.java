25633ebe6b1f649816b88099514e689a8bf4bb0d
Ryan <ryan@awkvi,com>
2020-08-31 12:18:45 -0700

    did initial values

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    // all in kJ
+    double specificHeatIce = 2.108;
+    double specificHeatWater = 4.184;
+    double specificHeatVapor = 1.996;
+    double LatentHeatFusion = .333;
+    double LatentHeatVaporization = 2.257;
+
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter Mass(In Grams):");
+        double mass = reader.nextDouble();
+        System.out.print("Enter Starting Temperature(In Celsius):");
+        double startingTemp = reader.nextDouble();
+        if (startingTemp < -273.14) {
+            startingTemp = -273.14;
+        }
+        startingPhaseCalculator(startingTemp);
+        System.out.print("Enter Ending Temperature(In Celsius):");
+        double endingTemp = reader.nextDouble();
+        if (endingTemp < -273.14) {
+            endingTemp = -273.14;
+        }
+        System.out.println("Mass: " + mass + "\nStarting Temperature: " + startingTemp + "C" + "\nEnding Temperature: " + endingTemp + "C");
+
+    }
+
+    public static String startingPhaseCalculator (double startingTemp) {
+        String startingPhase = new String();
+        if (startingTemp > 100) {
+            startingPhase = "vapor";
+        } else if (startingTemp < 100 && startingTemp > 0) {
+            startingPhase = "liquid";
+        } else {
+            startingPhase = "Ice";
+        }
+        return startingPhase;
     }
 }
